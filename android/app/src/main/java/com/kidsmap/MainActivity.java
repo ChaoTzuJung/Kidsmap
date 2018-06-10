fe41bc66f716b3a2dc2374fac17bcb3822c0c976
Alan Chao <s110319022@stu.ntue.edu.tw>
2018-06-11 00:01:38 +0800

    add google-font and android/iOS launch screen

--- android/app/src/main/java/com/kidsmap/MainActivity.java
+++ android/app/src/main/java/com/kidsmap/MainActivity.java
@@ -1,7 +1,29 @@
 package com.kidsmap;
 
 import com.reactnativenavigation.controllers.SplashActivity;
+import android.widget.LinearLayout;
+import android.graphics.Color;
+import android.view.Gravity;
+import android.util.TypedValue;
+import android.widget.TextView;
 
 public class MainActivity extends SplashActivity {
+
+    @Override
+    public LinearLayout createSplashLayout(){
+        LinearLayout view = new LinearLayout(this);
+        TextView text = new TextView(this);
+
+        view.setBackgroundColor(Color.parseColor("#00A6E7"));
+        view.setGravity(Gravity.CENTER);
+
+        text.setText("Kidsmap");
+        text.setTextColor(Color.parseColor("#FFE37F"));
+        text.setGravity(Gravity.CENTER);
+        text.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 60);
+
+        view.addView(text);
+        return view;
+    }
 
 }
---
+++ src/components/screens/Login/logo.js
@@ -0,0 +1,85 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, Animated, Easing } from 'react-native';
+
+class logo extends Component {
+
+    state = {
+        kidsAnim: new Animated.Value(0),
+        mapAnim: new Animated.Value(0),
+    }
+
+    componentWillMount() {
+        Animated.sequence([
+            Animated.timing(this.state.kidsAnim, {
+                toValue: 1,
+                duration: 1000,
+                easing: Easing.easeOutCubbic
+            }),
+            Animated.timing(this.state.mapAnim, {
+                toValue: 1,
+                duration: 500,
+                easing: Easing.easeOutCubbic
+            })
+        ]).start(() => {
+            alert('Done')
+        })
+    };
+
+    render() {
+        //偵測螢幕狀態
+        console.log(this.props.orientation)
+        return (
+            <View>
+                <View style={
+                    this.props.orientation === "protrait"
+                        ? styles.logoStylesPortait
+                        : styles.logoStylesLandscope
+                }>
+                    <Animated.View style={{
+                        opacity: this.state.kidsAnim,
+                        top: this.state.kidsAnim.interpolate({
+                            inputRange: [0, 1],
+                            outputRange: [100, 0]
+                        })
+
+                    }}>
+                        <Text style={styles.sell}>Kids</Text>
+                    </Animated.View>
+                    <Animated.View style={{
+                        opacity: this.state.mapAnim,
+                    }}>
+                        <Text style={styles.it}>map</Text>
+                    </Animated.View>
+                </View>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    logoStylesPortait: {
+        marginTop: 50,
+        flex: 1,
+        flexDirection: 'row',
+        maxHeight: 100
+    },
+    logoStylesLandscope: {
+        marginTop: 20,
+        flex: 1,
+        flexDirection: 'row',
+        maxHeight: 50
+    },
+    sell: {
+        fontSize: 40,
+        fontFamily: 'ChalkboardSE-Regular',
+        color: 'rgb(0,166,231)'
+    },
+    it: {
+        fontSize: 40,
+        fontFamily: 'ChalkboardSE-Regular',
+        color: 'rgb(255,227,127)'
+    }
+
+})
+
+export default logo;
